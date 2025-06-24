8ae45d65daf1123ab6472383edcaf28ee14d45d4
aurora591 <145192284+aurora591@users.noreply.github.com>
2025-06-24 12:47:10 +0300

    buggy total code

---
+++ buggy_code.py
@@ -0,0 +1,22 @@
+def average_lists(list1, list2):
+    """
+    this function gets two lists of numbers and return number of average element of both lists
+    :param list1: list of num
+    :param list2: list of num
+    :return: int of average element of both lists
+    """
+    new_list = list1 + list2
+    sum = 0
+    amount = 0
+    average = 0
+    for number in new_list:
+        sum += number
+        amount += 1
+        average = sum / amount
+    print("the average is " + str(average))
+
+def main():
+    list1 = [2, 3, 4, 8, 5, 13]
+    list2 = [66, 8, 2, 9, 12]
+    average_lists(list1, list2)
+main()
